5967c39f459061ec7c297180d0ee9334dbc2f61b
Ramprasadya <yadavramprasad563@gmail.com>
2022-07-07 13:14:06 +0530

    added borad state

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./componets/Board";
+import './styles/root.scss'
+function App() {
+  return (
+    <div className="app" >
+    <h1>TIC TAC TOE</h1>
+     <Board/>
+    </div>
+  );
+}
+
+export default App;
